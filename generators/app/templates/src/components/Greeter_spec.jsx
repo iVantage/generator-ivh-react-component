739a183907626e6b96b98e627bcc754f1f6521a6
unknown <jus.russell@gmail.com>
2016-09-30 14:15:02 -0400

    fix: Mocha suggests not using fat arrows for lexical scoping

--- generators/app/templates/src/components/Greeter_spec.jsx
+++ generators/app/templates/src/components/Greeter_spec.jsx
@@ -4,18 +4,18 @@ import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import <%= className %> from './<%= className %>'
 
-describe('<<%= className %> />', () => {
-  it('should greet the given moniker', () => {
+describe('<<%= className %> />', function() {
+  it('should greet the given moniker', function() {
     const wrapper = shallow(<<%= className %> moniker="iVantage" />)
     expect(wrapper.text()).to.equal('Hello, iVantage')
   })
 
-  it('should greet the world by default', () => {
+  it('should greet the world by default', function() {
     const wrapper = shallow(<<%= className %> />)
     expect(wrapper.text()).to.equal('Hello, World')
   })
 
-  it('should greet loudly', () => {
+  it('should greet loudly', function() {
     const wrapper = shallow(<<%= className %> />)
     expect(wrapper.is('h1')).to.equal(true)
   })
