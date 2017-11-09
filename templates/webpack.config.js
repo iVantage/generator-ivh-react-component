c938eae99cd286f1e094ec62d3a9e921db4d1d11
Justin Russell <jus.russell@gmail.com>
2017-11-09 16:24:16 -0500

    feature: List all peer deps ad external

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -4,6 +4,33 @@ const path = require('path')
 const srcPath = path.join(__dirname, 'src')
 const distPath = path.join(__dirname, 'dist')
 
+// Mark all peer dependencies as external
+const pkg = require('./package.json')
+const peerDeps = pkg.peerDependencies || {}
+const externals = Object.keys(peerDeps).reduce((acc, key) => {
+  acc[key] = {
+    commonjs: key,
+    commonjs2: key,
+    amd: key
+  }
+  return acc
+}, {})
+
+// React + ReactDOM can always be external
+externals.react = {
+  root: 'React',
+  commonjs: 'react',
+  commonjs2: 'react',
+  amd: 'react'
+}
+
+external['react-dom'] = {
+  root: 'ReactDOM',
+  commonjs: 'react-dom',
+  commonjs2: 'react-dom',
+  amd: 'react-dom'
+}
+
 module.exports = {
   entry: './src/index.js',
   resolve: {
@@ -15,20 +42,7 @@ module.exports = {
     path: distPath,
     filename: 'index.js'
   },
-  externals: {
-    react: {
-      root: 'React',
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react'
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom'
-    }
-  },
+  externals: externals,
   module: {
     rules: [{
       test: /\.js$/,
